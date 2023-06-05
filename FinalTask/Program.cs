8197cdd8eb0d45b97ee764266d2b9db05368cac0
Alexandr <k9108@mail.ru>
2023-06-05 12:30:35 +0300

    Finished first part of programm, print summ, next step - will create new Array

---
+++ FinalTask/Program.cs
@@ -0,0 +1,30 @@
+﻿
+string [] Array = new string [4];
+
+Array[0] = "xxx";
+Array[1] = "xx";
+Array[2] = "xx";
+Array[3] = "x";
+
+int summ = 0;
+
+for(int i = 0; i < 4; i++)
+{
+    if(Array[i].Length <= 2)
+    {
+        summ += 1;
+    }
+    System.Console.WriteLine(Array[i]);
+}
+
+System.Console.WriteLine("Кол-во ячеек в массиве <= 2 равно " + summ);
+
+
+
+
+
+
+
+
+
+
